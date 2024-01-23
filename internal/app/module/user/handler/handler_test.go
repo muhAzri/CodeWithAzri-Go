567824e86d0df255448cb6e9b35c88f83407452b
Azri <muhammad.azri.f.s@gmail.com>
2024-01-23 23:42:22 +0700

    test(user):test-get-profile-handler

--- internal/app/module/user/handler/handler_test.go
+++ internal/app/module/user/handler/handler_test.go
@@ -31,33 +31,24 @@ func TestHandler_Create(t *testing.T) {
 	userHandler, mockService := initializeHandler(t)
 
 	t.Run("Create User Successfully", func(t *testing.T) {
-		// Create a sample user input data
 		userInput := []byte(`{"name": "John Doe", "email": "john.doe@example.com", "profilePicture": "https://example.com/image.png"}`)
 
-		// Mock the service's Create method
 		mockService.On("Create", mock.AnythingOfType("*dto.CreateUpdateDto")).Return(dto.UserDTO{ID: "123", Name: "John Doe", Email: "john.doe@example.com"}, nil)
 
-		// Create a request with the sample user input
 		req, err := http.NewRequest("POST", "/create", bytes.NewBuffer(userInput))
 		assert.NoError(t, err)
 
-		// Set the user ID in the request context (you may need to update this based on your actual implementation)
-		// req = requestPkg.SetUserID(req, "user123")
 		patch := monkey.Patch(requestPkg.GetUserID, func(r *http.Request) string {
 			return "user123"
 		})
 		defer patch.Unpatch()
 
-		// Create a response recorder to record the response
 		recorder := httptest.NewRecorder()
 
-		// Call the handler's Create method
 		userHandler.Create(recorder, req)
 
-		// Assertions
-		assert.Equal(t, http.StatusOK, recorder.Code) // Check if the status code is OK
+		assert.Equal(t, http.StatusOK, recorder.Code)
 
-		// Parse the response body to check the response content
 		var response map[string]interface{}
 		err = json.Unmarshal(recorder.Body.Bytes(), &response)
 		assert.NoError(t, err)
@@ -65,10 +56,8 @@ func TestHandler_Create(t *testing.T) {
 		assert.Equal(t, "User Created/Fetched Successfully", response["meta"].(map[string]interface{})["message"])
 		assert.Equal(t, "Success", response["meta"].(map[string]interface{})["status"])
 
-		// Ensure that the user service's Create method was called with the correct input
 		mockService.AssertCalled(t, "Create", mock.AnythingOfType("*dto.CreateUpdateDto"))
 
-		// Reset the mock to prepare for the next test
 		mockService.AssertExpectations(t)
 	})
 }
@@ -89,8 +78,6 @@ func TestHandler_Create_DecodeError(t *testing.T) {
 
 		recorder := httptest.NewRecorder()
 
-		fmt.Println(recorder)
-
 		userHandler.Create(recorder, req)
 
 		assert.Equal(t, http.StatusBadRequest, recorder.Code)
@@ -102,8 +89,8 @@ func TestHandler_Create_Validator_Error(t *testing.T) {
 
 	userHandler, _ := initializeHandler(t)
 
-	t.Run("Create User Error Decoding", func(t *testing.T) {
-		userInput := []byte(`{"name": "John Doe", "email": "john.doe@example.com"`)
+	t.Run("Create User Error Validation", func(t *testing.T) {
+		userInput := []byte(`{"name": "John Doe", "email": "john.doe@example.com"}`)
 
 		req, err := http.NewRequest("POST", "/users", bytes.NewBuffer(userInput))
 		fmt.Println(err)
@@ -118,4 +105,91 @@ func TestHandler_Create_Validator_Error(t *testing.T) {
 
 	})
 
+}
+
+func TestHandler_Create_ServiceError(t *testing.T) {
+	userHandler, mockService := initializeHandler(t)
+
+	t.Run("Create User Service Error", func(t *testing.T) {
+		userInput := []byte(`{"name": "John Doe", "email": "john.doe@example.com", "profilePicture": "https://example.com/image.png"}`)
+
+		mockService.On("Create", mock.AnythingOfType("*dto.CreateUpdateDto")).Return(dto.UserDTO{ID: "123", Name: "John Doe", Email: "john.doe@example.com"}, errors.New("Internal Server Error"))
+
+		req, err := http.NewRequest("POST", "/create", bytes.NewBuffer(userInput))
+		assert.NoError(t, err)
+
+		patch := monkey.Patch(requestPkg.GetUserID, func(r *http.Request) string {
+			return "user123"
+		})
+		defer patch.Unpatch()
+
+		recorder := httptest.NewRecorder()
+
+		userHandler.Create(recorder, req)
+
+		assert.Equal(t, http.StatusInternalServerError, recorder.Code)
+
+		var response map[string]interface{}
+		err = json.Unmarshal(recorder.Body.Bytes(), &response)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Error", response["meta"].(map[string]interface{})["message"])
+		assert.Equal(t, "error", response["meta"].(map[string]interface{})["status"])
+
+		mockService.AssertCalled(t, "Create", mock.AnythingOfType("*dto.CreateUpdateDto"))
+
+		mockService.AssertExpectations(t)
+	})
+}
+
+func TestHandler_GetProfile(t *testing.T) {
+	userHandler, mockService := initializeHandler(t)
+
+	t.Run("Get User Profile Successfully", func(t *testing.T) {
+		mockService.On("GetProfile", mock.AnythingOfType("string")).Return(dto.UserProfileDTO{ID: "123", Name: "John Doe", ProfilePicture: "https://example.com/img.png"}, nil)
+
+		patch := monkey.Patch(requestPkg.GetUserID, func(r *http.Request) string {
+			return "user123"
+		})
+		defer patch.Unpatch()
+
+		req, err := http.NewRequest("GET", "/user/profile", nil)
+		assert.NoError(t, err)
+
+		recorder := httptest.NewRecorder()
+
+		userHandler.GetProfile(recorder, req)
+
+		assert.Equal(t, http.StatusOK, recorder.Code)
+	})
+}
+
+func TestHandler_GetProfile_Service_error(t *testing.T) {
+	userHandler, mockService := initializeHandler(t)
+
+	t.Run("Get User Profile Successfully", func(t *testing.T) {
+		mockService.On("GetProfile", mock.AnythingOfType("string")).Return(dto.UserProfileDTO{ID: "123", Name: "John Doe", ProfilePicture: "https://example.com/img.png"}, errors.New("Internal Server Error"))
+
+		patch := monkey.Patch(requestPkg.GetUserID, func(r *http.Request) string {
+			return "user123"
+		})
+		defer patch.Unpatch()
+
+		req, err := http.NewRequest("GET", "/user/profile", nil)
+		assert.NoError(t, err)
+
+		recorder := httptest.NewRecorder()
+
+		userHandler.GetProfile(recorder, req)
+
+		assert.Equal(t, http.StatusInternalServerError, recorder.Code)
+
+		var response map[string]interface{}
+		err = json.Unmarshal(recorder.Body.Bytes(), &response)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Error", response["meta"].(map[string]interface{})["message"])
+		assert.Equal(t, "error", response["meta"].(map[string]interface{})["status"])
+
+	})
 }
